9a4ca695e7aca382e995198258ad4b91aeaa039a
Carlos Mendes <carls.mendes@gmail.com>
2017-11-17 10:20:39 +0000

    entire ajax lecture

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,26 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :destroy]
+
   def index
     @restaurants = Restaurant.all
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
     @review = Review.new
+  end
+
+  def destroy
+    @restaurant.destroy
+
+    respond_to do |format|
+      format.html { redirect_to restaurants_path }
+      format.js  # <-- will render `app/views/reviews/destroy.js.erb`
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
